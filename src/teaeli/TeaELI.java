2d11aab71369ee918ed86da1f2babde7218d1453
SChamal <cham.saccha@gmail.com>
2016-02-16 15:25:42 +0530

    tabbed pane created for the admin home screen

--- src/teaeli/TeaELI.java
+++ src/teaeli/TeaELI.java
@@ -5,7 +5,9 @@ public class TeaELI {
 
     public static void main(String[] args) {
         LoginFrame lf = new LoginFrame();
-        lf.setVisible(true);
+        //lf.setVisible(true);
+        AdminPannel ad = new AdminPannel();
+        ad.setVisible(true);
     }
     
 }
