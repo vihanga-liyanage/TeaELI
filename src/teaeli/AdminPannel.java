e801160ccce616e55a47860a31a0843f9955f668
vihanga-liyanage <vihangaliyanage007@gmail.com>
2016-02-17 12:56:34 +0530

    Few look and feel updates

--- src/teaeli/AdminPannel.java
+++ src/teaeli/AdminPannel.java
@@ -6,6 +6,12 @@
 
 package teaeli;
 
+import java.awt.Font;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
 /**
  *
  * @author CHAM PC
@@ -16,12 +22,28 @@ public class AdminPannel extends javax.swing.JFrame {
      * Creates new form AdminPannel
      */
     public AdminPannel() {
+        try
+        {
+            setUIFont(new javax.swing.plaf.FontUIResource("Segoe UI", Font.PLAIN, 14));
+        }
+        catch(Exception e){}
+
         initComponents();
-        
-        //adding logo
 
     }
-
+    
+    //Setting default font
+    public static void setUIFont(javax.swing.plaf.FontUIResource f)
+    {   
+        java.util.Enumeration keys = UIManager.getDefaults().keys();
+        while(keys.hasMoreElements())
+        {
+            Object key = keys.nextElement();
+            Object value = UIManager.get(key);
+            if(value instanceof javax.swing.plaf.FontUIResource) UIManager.put(key, f);
+        }
+    }
+    
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
@@ -59,7 +81,11 @@ public class AdminPannel extends javax.swing.JFrame {
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("System Name");
+        setFont(new java.awt.Font("Segoe UI", 0, 15)); // NOI18N
 
+        jTabbedPane1.setFont(new java.awt.Font("Segoe UI Symbol", 0, 15)); // NOI18N
+
+        addNewOrderBtn.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
         addNewOrderBtn.setText("Add New Perchase Order");
 
         orderListTable.setModel(new javax.swing.table.DefaultTableModel(
@@ -84,17 +110,30 @@ public class AdminPannel extends javax.swing.JFrame {
             }
         ) {
             boolean[] canEdit = new boolean [] {
-                false, false, true, true
+                false, false, false, false
             };
 
             public boolean isCellEditable(int rowIndex, int columnIndex) {
                 return canEdit [columnIndex];
             }
         });
+        orderListTable.setRowHeight(20);
         jScrollPane2.setViewportView(orderListTable);
+        if (orderListTable.getColumnModel().getColumnCount() > 0) {
+            orderListTable.getColumnModel().getColumn(0).setResizable(false);
+            orderListTable.getColumnModel().getColumn(0).setPreferredWidth(40);
+            orderListTable.getColumnModel().getColumn(1).setResizable(false);
+            orderListTable.getColumnModel().getColumn(1).setPreferredWidth(50);
+            orderListTable.getColumnModel().getColumn(2).setResizable(false);
+            orderListTable.getColumnModel().getColumn(2).setPreferredWidth(80);
+            orderListTable.getColumnModel().getColumn(3).setResizable(false);
+            orderListTable.getColumnModel().getColumn(3).setPreferredWidth(200);
+        }
 
+        jLabel1.setFont(new java.awt.Font("Segoe UI Semibold", 0, 16)); // NOI18N
         jLabel1.setText("Available Orders");
 
+        jLabel2.setFont(new java.awt.Font("Segoe UI Semibold", 0, 16)); // NOI18N
         jLabel2.setText("Order Details");
 
         orderDetailsTable.setModel(new javax.swing.table.DefaultTableModel(
@@ -116,7 +155,22 @@ public class AdminPannel extends javax.swing.JFrame {
                 return canEdit [columnIndex];
             }
         });
+        orderDetailsTable.setRowHeight(20);
         jScrollPane3.setViewportView(orderDetailsTable);
+        if (orderDetailsTable.getColumnModel().getColumnCount() > 0) {
+            orderDetailsTable.getColumnModel().getColumn(0).setResizable(false);
+            orderDetailsTable.getColumnModel().getColumn(0).setPreferredWidth(200);
+            orderDetailsTable.getColumnModel().getColumn(1).setResizable(false);
+            orderDetailsTable.getColumnModel().getColumn(1).setPreferredWidth(40);
+            orderDetailsTable.getColumnModel().getColumn(2).setResizable(false);
+            orderDetailsTable.getColumnModel().getColumn(2).setPreferredWidth(40);
+            orderDetailsTable.getColumnModel().getColumn(3).setResizable(false);
+            orderDetailsTable.getColumnModel().getColumn(3).setPreferredWidth(60);
+            orderDetailsTable.getColumnModel().getColumn(4).setResizable(false);
+            orderDetailsTable.getColumnModel().getColumn(4).setPreferredWidth(60);
+            orderDetailsTable.getColumnModel().getColumn(5).setResizable(false);
+            orderDetailsTable.getColumnModel().getColumn(5).setPreferredWidth(60);
+        }
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
@@ -131,13 +185,13 @@ public class AdminPannel extends javax.swing.JFrame {
                     .addGroup(jPanel1Layout.createSequentialGroup()
                         .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addComponent(jLabel1)
-                            .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 371, javax.swing.GroupLayout.PREFERRED_SIZE))
+                            .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 500, javax.swing.GroupLayout.PREFERRED_SIZE))
                         .addGap(18, 18, 18)
                         .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(jScrollPane3, javax.swing.GroupLayout.DEFAULT_SIZE, 340, Short.MAX_VALUE)
+                            .addComponent(jScrollPane3, javax.swing.GroupLayout.PREFERRED_SIZE, 0, Short.MAX_VALUE)
                             .addGroup(jPanel1Layout.createSequentialGroup()
                                 .addComponent(jLabel2)
-                                .addGap(0, 0, Short.MAX_VALUE)))))
+                                .addGap(0, 160, Short.MAX_VALUE)))))
                 .addContainerGap())
         );
         jPanel1Layout.setVerticalGroup(
@@ -150,10 +204,10 @@ public class AdminPannel extends javax.swing.JFrame {
                     .addComponent(jLabel1)
                     .addComponent(jLabel2))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                    .addComponent(jScrollPane2, javax.swing.GroupLayout.DEFAULT_SIZE, 278, Short.MAX_VALUE)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jScrollPane2, javax.swing.GroupLayout.DEFAULT_SIZE, 365, Short.MAX_VALUE)
                     .addComponent(jScrollPane3, javax.swing.GroupLayout.PREFERRED_SIZE, 0, Short.MAX_VALUE))
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addContainerGap())
         );
 
         jTabbedPane1.addTab("Orders", jPanel1);
@@ -169,10 +223,12 @@ public class AdminPannel extends javax.swing.JFrame {
                 "Title 1", "Title 2", "Title 3", "Title 4"
             }
         ));
+        jTable1.setRowHeight(20);
         jScrollPane1.setViewportView(jTable1);
 
         searchItemBtn.setText("Go");
 
+        jLabel4.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
         jLabel4.setText("Start typing item name to search");
 
         javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
@@ -182,11 +238,11 @@ public class AdminPannel extends javax.swing.JFrame {
             .addGroup(jPanel2Layout.createSequentialGroup()
                 .addContainerGap()
                 .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 729, Short.MAX_VALUE)
+                    .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 774, Short.MAX_VALUE)
                     .addGroup(jPanel2Layout.createSequentialGroup()
                         .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addGroup(jPanel2Layout.createSequentialGroup()
-                                .addComponent(searchItemTextField, javax.swing.GroupLayout.PREFERRED_SIZE, 132, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addComponent(searchItemTextField, javax.swing.GroupLayout.PREFERRED_SIZE, 200, javax.swing.GroupLayout.PREFERRED_SIZE)
                                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                                 .addComponent(searchItemBtn))
                             .addComponent(jLabel4))
@@ -203,7 +259,7 @@ public class AdminPannel extends javax.swing.JFrame {
                     .addComponent(searchItemTextField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(searchItemBtn))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 383, Short.MAX_VALUE)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 370, Short.MAX_VALUE)
                 .addContainerGap())
         );
 
@@ -213,11 +269,11 @@ public class AdminPannel extends javax.swing.JFrame {
         jPanel3.setLayout(jPanel3Layout);
         jPanel3Layout.setHorizontalGroup(
             jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 749, Short.MAX_VALUE)
+            .addGap(0, 794, Short.MAX_VALUE)
         );
         jPanel3Layout.setVerticalGroup(
             jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 459, Short.MAX_VALUE)
+            .addGap(0, 452, Short.MAX_VALUE)
         );
 
         jTabbedPane1.addTab("Items", jPanel3);
@@ -240,6 +296,7 @@ public class AdminPannel extends javax.swing.JFrame {
                 return canEdit [columnIndex];
             }
         });
+        productTable.setRowHeight(20);
         jScrollPane4.setViewportView(productTable);
         if (productTable.getColumnModel().getColumnCount() > 0) {
             productTable.getColumnModel().getColumn(0).setMinWidth(100);
@@ -252,8 +309,10 @@ public class AdminPannel extends javax.swing.JFrame {
 
         searchProductBtn.setText("Go");
 
+        jLabel3.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
         jLabel3.setText("Start typing product name to search");
 
+        addProductBtn.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
         addProductBtn.setText("Add New Product");
 
         javax.swing.GroupLayout jPanel4Layout = new javax.swing.GroupLayout(jPanel4);
@@ -263,7 +322,7 @@ public class AdminPannel extends javax.swing.JFrame {
             .addGroup(jPanel4Layout.createSequentialGroup()
                 .addContainerGap()
                 .addGroup(jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jScrollPane4, javax.swing.GroupLayout.DEFAULT_SIZE, 729, Short.MAX_VALUE)
+                    .addComponent(jScrollPane4, javax.swing.GroupLayout.DEFAULT_SIZE, 774, Short.MAX_VALUE)
                     .addGroup(jPanel4Layout.createSequentialGroup()
                         .addGroup(jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addGroup(jPanel4Layout.createSequentialGroup()
@@ -288,8 +347,8 @@ public class AdminPannel extends javax.swing.JFrame {
                             .addComponent(searchProductBtn)))
                     .addComponent(addProductBtn))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(jScrollPane4, javax.swing.GroupLayout.PREFERRED_SIZE, 383, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addComponent(jScrollPane4, javax.swing.GroupLayout.DEFAULT_SIZE, 370, Short.MAX_VALUE)
+                .addContainerGap())
         );
 
         jTabbedPane1.addTab("Products", jPanel4);
@@ -320,7 +379,7 @@ public class AdminPannel extends javax.swing.JFrame {
                     .addComponent(logoLabel, javax.swing.GroupLayout.PREFERRED_SIZE, 63, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(timeLabel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                 .addGap(9, 9, 9)
-                .addComponent(jTabbedPane1)
+                .addComponent(jTabbedPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 487, Short.MAX_VALUE)
                 .addContainerGap())
         );
 
@@ -338,7 +397,7 @@ public class AdminPannel extends javax.swing.JFrame {
          */
         try {
             for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
+                if ("Metal".equals(info.getName())) {
                     javax.swing.UIManager.setLookAndFeel(info.getClassName());
                     break;
                 }
