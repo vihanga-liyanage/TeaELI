7ede64bd82e600cc69c0b68af4b2b12a1c0ce9ef
SChamal <cham.saccha@gmail.com>
2016-02-24 14:50:28 +0530

    user and stock history classes constructed

--- src/classes/User.java
+++ src/classes/User.java
@@ -1,14 +1,54 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package classes;
 
-/**
- *
- * @author ASHI
- */
 public class User {
+    private String userName, Password, firstName, lastName, designation;
+
+    public User() {
+        this.userName = "";
+        this.Password = "";
+        this.firstName = "";
+        this.lastName = "";
+        this.designation = "";
+    }
+    
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return Password;
+    }
+
+    public void setPassword(String Password) {
+        this.Password = Password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getDesignation() {
+        return designation;
+    }
+
+    public void setDesignation(String designation) {
+        this.designation = designation;
+    }
     
 }
