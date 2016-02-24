9dd67e115f693f46a2b9b5fd2e05f237bd90d12c
ashanthi93 <ashanthi1993@gmail.com>
2016-02-24 15:06:05 +0530

    blend class update and dbconnection class removed

--- src/classes/Blend.java
+++ src/classes/Blend.java
@@ -1,14 +1,77 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package classes;
 
-/**
- *
- * @author ASHI
- */
+import java.util.ArrayList;
+
 public class Blend {
+    private String blendID, blendName, baseName;
+    private int baseID, visibleStock, orderedStock, invisibleStock;
+    private ArrayList<Ingredient> ingredientArray = new ArrayList();
+    private ArrayList<Ingredient> flavourArray = new ArrayList();
+
+    public Blend() {
+        this.blendID = "";
+        this.blendName = "";
+        this.baseName = "";
+        this.baseID = 0;
+        this.visibleStock = 0;
+        this.orderedStock = 0;
+        this.invisibleStock = 0;
+    }
     
+    public String getBlendID() {
+        return blendID;
+    }
+
+    public void setBlendID(String blendID) {
+        this.blendID = blendID;
+    }
+
+    public String getBlendName() {
+        return blendName;
+    }
+
+    public void setBlendName(String blendName) {
+        this.blendName = blendName;
+    }
+
+    public String getBaseName() {
+        return baseName;
+    }
+
+    public void setBaseName(String baseName) {
+        this.baseName = baseName;
+    }
+
+    public int getBaseID() {
+        return baseID;
+    }
+
+    public void setBaseID(int baseID) {
+        this.baseID = baseID;
+    }
+
+    public int getVisibleStock() {
+        return visibleStock;
+    }
+
+    public void setVisibleStock(int visibleStock) {
+        this.visibleStock = visibleStock;
+    }
+
+    public int getOrderedStock() {
+        return orderedStock;
+    }
+
+    public void setOrderedStock(int orderedStock) {
+        this.orderedStock = orderedStock;
+    }
+
+    public int getInvisibleStock() {
+        return invisibleStock;
+    }
+
+    public void setInvisibleStock(int invisibleStock) {
+        this.invisibleStock = invisibleStock;
+    }
+   
 }
