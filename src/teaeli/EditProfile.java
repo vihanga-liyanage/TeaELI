0c324b6ed562a61e320970d60a6475f244c77328
ashanthi93 <ashanthi1993@gmail.com>
2016-03-17 11:46:19 +0530

    deliver blend,edit profile error lables done

--- src/teaeli/EditProfile.java
+++ src/teaeli/EditProfile.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package teaeli;
 
 import classes.User;
@@ -21,10 +17,7 @@ import javax.swing.JOptionPane;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
 
-/**
- *
- * @author Thisara Salgado
- */
+
 public class EditProfile extends javax.swing.JFrame {
     User us = new User();
     int pswrdFlag =0;
@@ -249,23 +242,23 @@ public class EditProfile extends javax.swing.JFrame {
         if(pswrdFlag==0){
             int result1 = us.updateUserName(firstname, lastname,username);
             if (result1 == 1){
-                JOptionPane.showMessageDialog(this, "Succsfully updated");
+                JOptionPane.showMessageDialog(this, "Succsfully updated","Update successfully",1);
                 this.setVisible(false);
             }
             else {
-                JOptionPane.showMessageDialog(this, "Error occurd while updating.. changes will not be saved");
+                JOptionPane.showMessageDialog(this, "Unable to update the details.Please try again.","Unable to Update",0);
             }
             
         }
         else if(pswrdFlag==1 && encriptCurentPswrd.isEmpty() && newpassword.isEmpty() && confirmpassword.isEmpty()){
             int result1 = us.updateUserName(firstname, lastname,username);
             if (result1 == 1){
-                JOptionPane.showMessageDialog(this, "Succsfully updated");
+                JOptionPane.showMessageDialog(this, "Succsfully updated","Update successfully",1);
                 this.setVisible(false);
             }
             else {
-                JOptionPane.showMessageDialog(this, "Error occurd while updating.. changes will not be saved");
-            }
+                 JOptionPane.showMessageDialog(this, "Unable to update the details.Please try again.","Unable to Update",0);
+              }
             
         }
             
@@ -273,12 +266,12 @@ public class EditProfile extends javax.swing.JFrame {
         else if(pswrdFlag==1 && !currentpassword.isEmpty() && !newpassword.isEmpty() && !confirmpassword.isEmpty() && newpassword.equals(confirmpassword)){
            int result2 = us.updatePassword(firstname, lastname, username, encriptNewPassword, encriptCurentPswrd);
             if(result2==1){
-                JOptionPane.showMessageDialog(this, "Password succsfully updated");
+                JOptionPane.showMessageDialog(this, "Password succsfully updated","successfully Updated",0);
                 this.setVisible(false);
             }
             else {
-                JOptionPane.showMessageDialog(this, "Error occurd while updating.. changes will not be saved");
-            }
+                 JOptionPane.showMessageDialog(this, "Unable to update the details.Please try again.","Unable to Update",0);
+               }
         }
         
     }//GEN-LAST:event_SaveChangesActionPerformed
