2b0dd2ce59df75a0c6e7abd2ef6a5dec54c9ef5e
ashanthi93 <ashanthi1993@gmail.com>
2016-03-17 12:07:44 +0530

    orderConfirmation,orderDetails error lables done

--- src/teaeli/ManagerPannel.java
+++ src/teaeli/ManagerPannel.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
 package teaeli;
 
@@ -27,10 +22,7 @@ import javax.swing.UnsupportedLookAndFeelException;
 import static teaeli.TeaELI.loginFrame;
 
 
-/**
- *
- * @author CHAM PC
- */
+
 public class ManagerPannel extends javax.swing.JFrame {
     User user = new User(); 
     
